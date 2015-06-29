1f8de3f1bc4cceb11a0d054057b0e6d87c1253cc
TiNico22 <TiNico22@users.noreply.github.com>
2015-06-30 00:54:45 +0200

    Create startELK.bat

---
+++ startELK.bat
@@ -0,0 +1,15 @@
+@echo off
+rem set java home dir
+SETLOCAL JAVA_HOME
+SET JAVA_HOME=%~dp0jre1.8.0
+echo %JAVA_HOME
+echo starting the ELK stack ... please wait a few seconds ....
+echo starting Elastiscsearch
+start "Elasticsearch" %~dp0elasticsearch-1.6\bin\elasticsearch.bat
+echo starting Kibana
+start "Kibana" %~dp0kibana-4.1\bin\kibana.bat
+echo starting logstash
+set JRUBY_BIN=%~dp0logstash-1.5\vendor\bundle\jruby\1.9\bin
+start "Logstash" %~dp0logstash-1.5\bin\logstash.bat -f %~dp0etc\logstash\conf.d
+echo Time to play, enjoy
+ENDLOCAL
