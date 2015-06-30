666a4140c0f7f5ac924e42e77ab21795034728f0
TiNico22 <TiNico22@users.noreply.github.com>
2015-07-01 01:11:50 +0200

    Update startELK.bat

--- startELK.bat
+++ startELK.bat
@@ -9,7 +9,6 @@ start "Elasticsearch" %~dp0elasticsearch-1.6\bin\elasticsearch.bat
 echo starting Kibana
 start "Kibana" %~dp0kibana-4.1\bin\kibana.bat
 echo starting logstash
-set JRUBY_BIN=%~dp0logstash-1.5\vendor\bundle\jruby\1.9\bin
 start "Logstash" %~dp0logstash-1.5\bin\logstash.bat -f %~dp0etc\logstash\conf.d
 echo Time to play, enjoy
 ENDLOCAL
